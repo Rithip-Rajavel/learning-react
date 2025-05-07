2b3aea5d96b9fadcdfa6ff566c8c2e4c0fe0ed2a
Rithip-Rajavel <141886703+Rithip-Rajavel@users.noreply.github.com>
2025-05-07 15:34:16 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+const Navbar =()=>{
+  return <h1>This is navbar</h1>
+}
+const Sidebar = () =>{
+  return <h1>This is Sidebar</h1>
+}
+const Articallist =() =>{
+  return <h1>This is Articals List</h1>
+}
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Sidebar />
+      <Articallist />
     </div>
   );
 }
