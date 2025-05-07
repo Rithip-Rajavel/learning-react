99edf78151fbb90f797074f6ea3cbd79761c1ae5
Rithip-Rajavel <141886703+Rithip-Rajavel@users.noreply.github.com>
2025-05-07 15:34:44 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-
+import Test from './components/components.js';
 const Navbar =()=>{
   return <h1>This is navbar</h1>
 }
@@ -10,12 +10,41 @@ const Sidebar = () =>{
 const Articallist =() =>{
   return <h1>This is Articals List</h1>
 }
-function App() {
+const Car =(probs) =>{
+  return <h1>This is Car {probs.brand.name}</h1>
+}
+
+const Item =({name, isPacked}) =>{
+  if(isPacked){
+    return <li className='item'>{name}✔</li>
+  }
+  return <li className='item'>{name}❌</li>
+}
+const list = [
+  {name:"item1", isPacked:true},
+  {name:"item2", isPacked:false},
+  {name:"item3", isPacked:true},
+  {name:"item4", isPacked:false},
+]
+const List =() =>{
   return (
-    <div className="App">
+    <ul>
+      {list.map((item) => (
+        <Item name={item.name} isPacked={item.isPacked} />
+      ))}
+    </ul>
+  )
+}
+function App() {
+  const carInfo = {name:"BMW" ,model :"X5", year:2023};
+  return (
+    <div >
       <Navbar />
       <Sidebar />
       <Articallist />
+      <Test />
+      <Car brand = {carInfo} />
+      <List />
     </div>
   );
 }
